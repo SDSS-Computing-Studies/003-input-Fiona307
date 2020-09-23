b5bc767b11fcb2f8f1a5d2e0af7fbe325488d1de
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-09-23 14:56:49 -0700

    Update task5.py

--- task5.py
+++ task5.py
@@ -13,3 +13,10 @@
 # Note: You will need to do some strange things with your cube root.
 # Remember that a cube root is the same as an exponent of 1/3, but
 # here you will need to do a power of 1.0/3 or something strange happens.
+ 
+import math
+
+volume = input("Enter the volume of a sphere ")
+radius = (float(volume)/float(math.pi)*float(3/4))**float(1.0/3)
+
+print("The radius of the sphere is " + str(radius))
